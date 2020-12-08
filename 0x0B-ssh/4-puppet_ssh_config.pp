23341b611b66320919f6faa50cf66439c7ac727d
Deyber2000 <garzondeiber6@gmail.com>
2020-12-08 13:51:37 -0500

    configuration management for server

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Turns off password authentication and uses holberton key
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
